1e1c9a84df9c828beeb3c3fed4ba67a7eac5aac1
omprakashbhanarkar <omprakashbhanarkar19@gmail.com>
2024-02-21 18:07:16 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,5 +1,57 @@
-resource "null_resource" "jenkins_pipeline" {
-  provisioner "local-exec" {
-    command = "./create_jenkins_job.sh"
-  }
+provider "jenkins" {
+  url      = "http://52.90.131.170:8080/"
+  username = "admin"
+  password = "admin123"
+}
+
+resource "jenkins_job" "pipeline" {
+  name     = "my-pipeline-job"
+  config   = <<EOF
+<flow-definition plugin="workflow-job@2.40">
+  <actions>
+    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobAction plugin="pipeline-model-definition@1.8.1"/>
+    <org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction plugin="pipeline-model-definition@1.8.1">
+      <jobProperties/>
+      <triggers/>
+      <parameters/>
+      <options/>
+    </org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction>
+  </actions>
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <properties>
+    <org.jenkinsci.plugins.workflow.job.properties.DisableConcurrentBuildsJobProperty/>
+    <com.coravy.hudson.plugins.github.GithubProjectProperty plugin="github@1.33.0">
+      <projectUrl>https://github.com/omprakashbhanarkar19/Sample_project.git</projectUrl>
+      <displayName></displayName>
+    </com.coravy.hudson.plugins.github.GithubProjectProperty>
+  </properties>
+  <definition class="org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition" plugin="workflow-cps@2.89">
+    <scm class="hudson.plugins.git.GitSCM" plugin="git@4.12.0">
+      <configVersion>2</configVersion>
+      <userRemoteConfigs>
+        <hudson.plugins.git.UserRemoteConfig>
+          <url>https://github.com/omprakashbhanarkar19/Sample_project.git</url>
+          <credentialsId>your-github-credentials-id</credentialsId>
+        </hudson.plugins.git.UserRemoteConfig>
+      </userRemoteConfigs>
+      <branches>
+        <hudson.plugins.git.BranchSpec>
+          <name>*/main</name>
+        </hudson.plugins.git.BranchSpec>
+      </branches>
+      <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
+      <submoduleCfg class="list"/>
+      <extensions/>
+    </scm>
+    <scriptPath>Jenkinsfile</scriptPath>
+  </definition>
+  <triggers>
+    <com.cloudbees.jenkins.GitHubPushTrigger plugin="github@1.33.0">
+      <spec></spec>
+    </com.cloudbees.jenkins.GitHubPushTrigger>
+  </triggers>
+  <disabled>false</disabled>
+</flow-definition>
+EOF
 }
