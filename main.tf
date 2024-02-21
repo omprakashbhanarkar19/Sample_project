4b84e6261bd91c136fb4a42914d6526c688a11e9
omprakashbhanarkar <omprakashbhanarkar19@gmail.com>
2024-02-21 20:38:27 +0530

    updated1

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 resource "null_resource" "create_jenkins_job" {
+  
+   triggers = {
+    always_run = "${timestamp()}"
+  }
+  
   provisioner "local-exec" {
     cammand = "./create_jenkins_job.sh"
 }
+
 }
