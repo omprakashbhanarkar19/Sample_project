66cf47a5093016bc2b9922c5b08e6a05cea7c2ca
omprakashbhanarkar <omprakashbhanarkar19@gmail.com>
2024-02-21 20:21:54 +0530

    updated1

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 resource "null_resource" "jenkins_pipeline" {
   provisioner "remote-exec" {
-    command = "./create_jenkins_job.sh"
-  }
+    inline = [
+       "sh create_jenkins_job.sh"
+    ]
+}
 }
