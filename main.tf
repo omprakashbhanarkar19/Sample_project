85cca4f4deef358f93cb873eb4930c79c3bc6d4f
omprakashbhanarkar <omprakashbhanarkar19@gmail.com>
2024-02-21 18:26:21 +0530

    updated

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-    required_providers {
-        jenkins = {
-            source = "registry.terraform.io/hashicorp/jenkins"
-            version = "~> 2.0.0"
-        }
+  required_providers {
+    jenkins = {
+      source = "hashicorp/jenkins"
+      version = "~> 2.0.0"
     }
+  }
 }
 
 provider "jenkins" {
